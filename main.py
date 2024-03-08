12f157a301eab696803a1cccbdf04ea887c13ca3
PetrosanW <ruslannikolaev5060@gmail.com>
2024-03-08 03:39:39 +0300

    Привет

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        die = False
         self.health = 100
         self.money = 0
         self.data = datetime.strptime("01.01.2024", "%d.%m.%Y")
@@ -28,20 +27,28 @@ class MainWindow(QMainWindow):
 
     def die_shahir(self):
         if self.health <= 0:
-            QtWidgets.QMessageBox.critical(None, "Произошла трагедия", "Шачир не выжил в этом жёстоком мире")
+            QtWidgets.QMessageBox.critical(None, "Финал: Шачир умер", "Шачир не выжил, он пытался найти денег на еду но не судьба. Он умер от голода")
             sys.exit()
+        elif self.money <= -1000:
+            QtWidgets.QMessageBox.critical(None, "Финал: Шачир уехал", "Шачир не нашёл способ как заработать денег и влез в долги. Он обратно уехал к себе в башкирию")
+            sys.exit()
+        elif self.money >= 1000000:
+            QtWidgets.QMessageBox.information(None, "Финал: Богатый Шачир", "Шачир заработал свой 1 миллион и стал крупным бизнесменом")
+            sys.exit()
+
 
     def static(self):
         self.ui.label.setText(f"Здоровье: {self.health}")
         self.ui.label_2.setText(f"Монет: {self.money}")
         self.ui.label_3.setText(f"Дата: {self.data.strftime('%d.%m.%Y')}")
+
     def healthadd(self):
         self.die_shahir()
         if self.health < 100:
             self.health += 10
             if self.health > 100:
                 self.health = 100
-            self.data += timedelta(days=1)
+            self.data += timedelta(days=random.randint(2, 4))
         elif self.health == 100:
             self.ui.label_4.setText(f"У тебя полное здоровье")
         self.static()
@@ -52,17 +59,17 @@ class MainWindow(QMainWindow):
         self.ui_dialog = Ui_Dialog()
         self.ui_dialog.setupUi(self.dialog)
         self.dialog.show()
-        self.ui_dialog.DpushButton.clicked.connect(self.Rob_5)  # Ограбить пятерочку
-        # self.ui_dialog.DpushButton_2.clicked.connect()  # в разработке
-        # self.ui_dialog.DpushButton_3.clicked.connect()  # в разработке
+        self.ui_dialog.DpushButton.clicked.connect(self.Rob)  # Ограбить пятерочку
+        self.ui_dialog.DpushButton_2.clicked.connect(self.temka)  # в Мутить темки
+        self.ui_dialog.DpushButton_3.clicked.connect(self.karman)  # в разработке
         self.static()
 
-    def Rob_5(self):
+    def Rob(self):
         self.die_shahir()
-        lost_money = random.randint(10, 100)
+        lost_money = random.randint(10, 50)
         lost_health = random.randint(5, 20)
-        if random.random() < 0.5:
-            if random.random() < 0.5:
+        if random.random() < 0.4:
+            if random.random() < 0.2:
                 self.ui.label_4.setText(f"Тебя поймал охранник, он сдал тебя ментам. Ты отсидел 15 суток, потерял {lost_health} и отдал {lost_money} монет")
                 self.money -= lost_money
                 self.health -= lost_health
@@ -70,12 +77,29 @@ class MainWindow(QMainWindow):
                 self.ui.label_4.setText(f"Тебя поймал охранник, он тебя отпустил но ты отдал {lost_money} монет")
                 self.money -= lost_money
         else:
-            add_money = random.randint(10, 1000)
+            add_money = random.randint(5, 140)
             self.money += add_money
             self.ui.label_4.setText(f"Тебя никто не поймал, ты наворовал на {add_money} монет")
 
         self.data += timedelta(days=1)
         self.static()
+
+    def temka(self):
+        add_money = random.randint(-500, 5000)
+        price = random.randint(500, 1000)
+        if self.money >= price:
+            self.money -= price
+            self.money += add_money
+            self.data += timedelta(days=random.randint(1, 10))
+            self.ui.label_4.setText(f"Ты купил темку за {price}, и заработал {add_money} монет")
+        else:
+            self.ui.label_4.setText(f"Не хватает для темки {price - self.money} монет")
+        self.static()
+
+    def karman(self):
+        QtWidgets.QMessageBox.information(None, "В разработке...", "Думаю что сюда добавить")
+
+
 
     def skiptime(self):  # Неготова
         self.die_shahir()
